a33944a54caeef2d8687365af80e76b997057c6b
Ismael Celis <ismaelct@gmail.com>
2012-06-05 19:20:50 +0100

    Log received client id (@cid)

--- lib/tecepe/connection.rb
+++ lib/tecepe/connection.rb
@@ -10,6 +10,7 @@ module Tecepe
     def post_init
       puts '[conn]'
       @heartbeat = setup_heartbeat
+      @cid = nil
       super
     end
 
@@ -20,24 +21,21 @@ module Tecepe
     end
 
     def receive_data data
-      puts "[rcvd] #{data}"
-      if data =~ /quit/i
-        close_connection_after_writing
-      else
-        begin
-          json = JSON.parse(data)
-          Tecepe.dispatch self, json['event'], json['payload']
-        rescue JSON::ParserError => e
-          send_error e.message
-        rescue Encoding::UndefinedConversionError => e
-          send_error e.message
-        end
-      end
+       begin
+         json = JSON.parse(data)
+         @cid = json['cid']
+         puts "[rcvd #{@cid}] #{json['payload']}"
+         Tecepe.dispatch self, json['event'], json['payload']
+       rescue JSON::ParserError => e
+         send_error e.message
+       rescue Encoding::UndefinedConversionError => e
+         send_error e.message
+       end
     end
 
     def reply(payload = {}, status = 1)
       json = JSON.generate(status: status, payload: payload)
-      puts "[repl] #{json}"
+      puts "[repl #{@cid}] #{json}"
       if error?
         puts "[error]"
       else
