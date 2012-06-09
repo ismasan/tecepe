b816ad0995eefa89a6a64c5da41dea6a3466b88d
Ismael Celis <ismaelct@gmail.com>
2012-06-09 22:50:20 +0100

    Example client sends multiline strings

--- examples/blocking_client.rb
+++ examples/blocking_client.rb
@@ -6,7 +6,14 @@ socket = Tecepe::BlockingClient.new('localhost', 5555)
 
 delay = ARGV[0]
 
+m = <<-eos
+Hello this is a text
+
+with line changes
+And more
+eos
+
 20.times do |i|
-  response = socket.call 'perms', delay: delay.to_f         # Send request
-  puts "RESPONSE: #{response}"
+  response = socket.call 'perms', delay: delay.to_f , m: m        # Send request
+  puts "RESPONSE: #{response.class.name} - #{response}"
 end
